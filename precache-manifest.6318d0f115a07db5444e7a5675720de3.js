a847c8a9d066913b9b771c455628993908e1aee9
Miguel Lapa <miguellapa30@ua.pt>
2019-05-27 19:26:51 +0100

    Updates

---
+++ precache-manifest.6318d0f115a07db5444e7a5675720de3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e1d6468e8ef46b1e27dabe5e443ff62",
+    "url": "/itchio/index.html"
+  },
+  {
+    "revision": "05e86d3729a93f86051e",
+    "url": "/itchio/static/css/main.9f3eb1be.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/itchio/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "05e86d3729a93f86051e",
+    "url": "/itchio/static/js/main.7ca72a41.chunk.js"
+  },
+  {
+    "revision": "0509c9840f2dfd00f0bb",
+    "url": "/itchio/static/js/runtime~main.75fd9d36.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/itchio/static/media/logo.5d5d9eef.svg"
+  }
+]);
